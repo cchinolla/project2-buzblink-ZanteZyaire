156f07a1ad2e706c0efdbcea6030fc9377226e4a
ZanteZyaire <jazamora6@miners.utep.edu>
2020-11-01 21:53:14 -0700

    Changed functionality of switch_interrupt handler()

--- project/src/switches.c
+++ project/src/switches.c
@@ -28,9 +28,9 @@ void
 switch_interrupt_handler()
 {
   char p2val = switch_update_interrupt_sense();
-  state = (p2val & SW1) ? 0 : 1;
-  state = (p2val & SW2) ? state : 2;
-  state = (p2val & SW3) ? state : 3;
-  state = (p2val & SW4) ? state : 4;
-  state_advance();
+  char state = (p2val & SW1) ? 0 : 1;
+  if (state == 0) state = (p2val & SW2) ? 0 : 2;
+  if (state == 0) state = (p2val & SW3) ? 0 : 3;
+  if (state == 0) state = (p2val & SW4) ? 0 : 4;
+  setButtonPress(state);
 }
